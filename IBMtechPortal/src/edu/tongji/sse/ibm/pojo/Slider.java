4e297fc452f3082ff001f95962ce18319f6895d7
putin <jiangwish@gmail.com>
2013-05-27 14:10:18 +0800

    数据库更改

--- IBMtechPortal/src/edu/tongji/sse/ibm/pojo/Slider.java
+++ IBMtechPortal/src/edu/tongji/sse/ibm/pojo/Slider.java
@@ -7,27 +7,35 @@ public class Slider {
 	private String name;
 	private String profile;
 	private Set<Pic> pics;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getProfile() {
 		return profile;
 	}
+
 	public void setProfile(String profile) {
 		this.profile = profile;
 	}
+
 	public Set<Pic> getPics() {
 		return pics;
 	}
+
 	public void setPics(Set<Pic> pics) {
 		this.pics = pics;
 	}
